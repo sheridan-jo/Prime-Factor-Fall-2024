922e8e55591763a7bc8673a0c99a7d786bee9d12
sheridan-jo <jsolenski@hotmail.com>
2024-10-08 20:18:38 -0400

    Add handling of non-integer input
    - Add conditional statement to check that argument for generate_prime_factors() is an integer
    - Add test to verify that a ValueError is raised when the function is called wtih non-integer input

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,26 @@
+"""
+Python Development II
+Assignment 3 - Prime Factors
+Test Suite for prime.py module
+John O.
+October 8, 2024
 
+This test suite ensures that the function, generate_prime_factors() in the module, prime.py, works correctly. It
+verifies that the function can handle any integer entered, including numbers less than two. It also verifies that it
+can handle errors resulting from non-integer input.
+
+Usage:
+Run this test module using pytest.
+
+Documented according to Google Style Docstrings
+https://google.github.io/styleguide/pyguide.html
+
+"""
+
+import pytest
+import prime
+
+#  Verify that ValueError is raised when generate_prime_factors() is called with non-integer argument
+def test_non_integer():
+    with pytest.raises(ValueError):
+        prime.generate_prime_factors(3.14)
--- prime.py
+++ prime.py
@@ -1,3 +1,32 @@
 """
-prime.py -- Write the application code here
+Python Development II
+Assignment 3 - Prime Factors
+John O.
+October 8, 2024
+
+This program contains a function that generates the prime factors of the number entered. 
+
+Usage:
+Enter an integer as the argument for the generate_prime_factors() function, and it will return
+a list containing the set of prime numbers which are multiplied by one another to form the
+number entered.
+
+Documented according to Google Style Docstrings
+https://google.github.io/styleguide/pyguide.html
+
 """
+
+def generate_prime_factors(number):
+    """
+        Generates a list of the prime factors for the number that the user inputs.
+
+        Parameters:
+        number(int): The number for which prime factors are generated. It must be an integer greater than one.
+
+        Returns:
+        list of int: A list of the prime numbers that multiply together to make 'number'.
+    """
+
+    #  Raises a ValueError if 'number' is not an integer
+    if not isinstance(number, int):
+        raise ValueError(f'{number} is not an integer.')
